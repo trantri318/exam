4eeccc9ecab3b3a992f81bb96bac258aafc41cd4
“Tri” <trantrilong318@gmail.com>
2019-06-25 10:44:12 +0700

    Initial commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,3 @@
 class Product < ActiveRecord::Base
-  before_save :strip_html_from_description
-  def strip_html_from_description
-     self.description =
-     ActionView::Base.full_sanitizer.sanitize(self.description)
-  end
+ belongs_to :category
 end
